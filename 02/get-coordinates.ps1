ffd0c91b6727d7a134d7aeac64cfe175c8e5c0bf
AlexFundell <71392715+AlexFundell@users.noreply.github.com>
2022-06-04 14:02:03 +0200

    Update for part 2 of day 02

--- 02/get-coordinates.ps1
+++ 02/get-coordinates.ps1
@@ -11,7 +11,8 @@ $coordinates = get-content $file
 $h_pos = 0
 #depth position 
 $d_pos = 0
-
+#aim 
+$aim = 0
 
 foreach ($coordinate in $coordinates){
     
@@ -20,14 +21,28 @@ foreach ($coordinate in $coordinates){
     $direction = ($coordinate) -replace '\d'
     $direction = $direction.trim()
 
-    write-verbose "going $direction by ""$distance"" units" 
- 
+    
+
     If ($direction -eq 'forward') {
         $h_pos = $h_pos + $distance 
+        If ($aim -eq 0){
+            write-verbose "aim is 0, just going forward"
+        }
+        Else {
+            $depthrate = ($aim * $distance)
+            $d_pos = $d_pos + $depthrate
+            write-verbose "going forward by $distance"
+            write-verbose "aim is $aim, going down by $depthrate units"
+        }
     } elseif ($direction -eq 'down') {
-        $d_pos = $d_pos + $distance 
+        write-verbose "aim increasing by $distance"
+        $aim = $aim + $distance
+        write-verbose "aim now at $aim"
     } elseif ($direction -eq 'up') {
-        $d_pos = $d_pos - $distance
+        write-verbose "aim decreasing by $distance"
+        $aim = $aim - $distance
+        write-verbose "aim now at $aim"
+
     }
 
     write-verbose "horizontal position: $h_pos"
